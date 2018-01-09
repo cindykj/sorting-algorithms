1d07daa65bebefe862480d2678625926b62ff7e6
KanKan <cindykan@KanKan.local>
2018-01-08 18:31:17 -1000

    Quick completed

--- sort-algorithms.js
+++ sort-algorithms.js
@@ -3,64 +3,39 @@ module.exports = function () {
 
   return {
     quick: quick,
-    merge: merge,
+    // merge: merge,
   }
 };
 
+//quick sort
+function quick(arr) {
+  if (arr.length < 1) {
+    return arr;
+  }
 
-  //consider using a swap function
-  // function swap (a, b) {
+  let pivot = arr[0];
+  let lesser = [];
+  let greater = [];
 
-  // }
-
-  //quick sort
-  function quick (arr) {
-    if (arr.length <= 1) {
-      return arr;
+  for (let i = 1; i < arr.length; i++) {
+    // if (arr[i] < pivot) {
+    if (arr[i] < arr[0]) {
+      lesser.push(arr[i]);
+    } else {
+      (arr[i] > arr[0])
+      greater.push(arr[i]);
     }
-
-    let pivot = arr[0];
-    let lesser = [];
-    let greater = [];
-
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < arr[0]) {
-      // if (arr[i] < pivot) {
-        lesser.push(arr[i]);
-      } else {
-        arr[i] > arr[0];
-        greater.push(arr[i]);
-      }
-      
-    }
-
-    console.log('pivot ' + pivot);
-    console.log('lesser ' + lesser);
-    console.log('greater ' + greater);
-
-    let final = lesser.concat(pivot, greater);
-    console.log('final array ' + final);
-
-    // console.log(lesser);
-    // console.log(greater);
-    // console.log(quick(lesser).concat(pivot, quick(greater)));
-    // console.log(quick(arr.slice()));
-  };
-  // console.log(quick(arr.slice()));
-
-
-
-
-  //merge sort
-  function merge(arr) {
-
-
-
-
 
   }
 
+// console.log('pivot ' + pivot);
+// console.log('lesser ' + lesser);
+// console.log('greater ' + greater);
 
+// let final = lesser.concat(pivot, greater);
+// console.log('final array ' + final);
 
+console.log(quick(lesser).concat(pivot, quick(greater)));
+return quick(lesser).concat(pivot, quick(greater));
 
-
+}
